ac74953f041dd0e9515516bc0bfb3ec9ba7f9d54
ChristiantoKape <christiantokape@gmail.com>
2022-06-25 00:32:29 +0700

    slicing login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function () {
+    return view('login');
+})->name('login');
+
+// Route::get('checkout', function () {
+//     return view('checkout');
+// })->name('checkout');
